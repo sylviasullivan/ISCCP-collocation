374c2d753c637599a7666d59391f9fc1c07f4172
Sylvia S <43764733+scs2229@users.noreply.github.com>
2019-06-02 13:52:23 -0400

    precipCollocate5_local.py, output array of depth, SST, precip intensity
    core collocated as precipCollocate4_local.py, output format of trop_SST_pcore_endjf_local_cores.npy, trop_SST_pcore_lndjf_local_cores.npy

--- precipCollocate.py
+++ precipCollocate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env mcsplot
-# SAVE THE VECTOR OF (1) MCS DEPTH [K] (2) SST [K] 
-# (3) MEAN PRECIP INTENSITY [mm h-1] AND (4) MAX PRECIP INTENSITY [mm h-1]
+# SAVE THE VECTOR OF (1) MCS DEPTH [K] (2) SST IN CONVECTIVE CORE [K] 
+# (3) PRECIP INTENSITY IN CONVECTIVE CORE [mm h-1] 
 # IN A VECTOR FOR THE PHASE DESIGNATED
 # BY ENSO AND FOR THE SUBDOMAIN INDICATED BY lowlat-hilat, lowlon-hilon
 
@@ -8,7 +8,7 @@
 # domain = 'am', 'wa', 'cp'
 # [-5, 5, 165, 190]; [-25, 5, 290, 325]; [0, 20, -10, 50]
 
-def precipCollocate4_local(ENSO):
+def precipCollocate5_local(ENSO):
     import time,sys,math
     import numpy as np
     import numpy.ma as ma
@@ -81,8 +81,8 @@ def precipCollocate4_local(ENSO):
     DD[:,1]  = CTdata[:,8]       # months
     DD[:,2]  = CTdata[:,9]       # days
     DD[:,3]  = CTdata[:,10]      # hours
-    DD[:,4]  = CTdata[:,12]      # lat
-    DD[:,5]  = CTdata[:,13]      # lon
+    DD[:,4]  = CTdata[:,21]      # core lat, 12 = sys lat
+    DD[:,5]  = CTdata[:,22]      # core lon, 13 = sys lon
     DD[:,6]  = CTdata[:,24]      # depth
     DD[:,7]  = CTdata[:,18]      # number of convective cores
     DD[:,8]  = CTdata[:,40]      # min lat
@@ -125,7 +125,7 @@ def precipCollocate4_local(ENSO):
     DD2[:,3] = np.asarray([CTtimes[jj].astype(object).hour for jj,_ in enumerate(CTtimes)])
  
     # calculate and save an array of precipitation intensities collocated with MCS
-    sea = []; pmean = []; pmax = []; dep = []
+    sea = []; precip = []; dep = []
     for ii in range(len(yrs)):
         print(str(yrs[ii]) + mon[ii])
         ppp   = []
@@ -143,29 +143,17 @@ def precipCollocate4_local(ENSO):
             indx   = reduce(np.intersect1d,(indx1,indx2,indx3,indx4))  
             subset = DD2[indx,:]
 
-            # extract the maximum and minimum lat and lon of the system
+            # extract the values at the closest 0.5 deg to the core lat, lon
             for kk in range(len(indx)):
-                lat = [int(round(subset[kk,8]*2)+180), int(round(subset[kk,9]*2)+180)]
-                lon = [int(round(subset[kk,10]*2)), int(round(subset[kk,11]*2))]
-                lon[lon == 720] = 0
-              
-                # define a grid with [lat] x [lon] dimensions
-                grid = np.zeros((len(lat),len(lon)))
-
-                # and store precipitation values for the max/min-lat/lon domain
-                pr = prec[jj,:,:] 
-                for xx in range(len(lat)):
-                    rr = lat[xx]
-                    if rr == 360:
-                       rr = 0
-                    ss = lon[0]
-                    for yy in range(len(lon)):
-                        ss = lon[yy]
-                        if(ss == 720):
-                           ss = 0
-                        grid[xx,yy] = pr[rr,ss]
+                #lat = [int(round(subset[kk,8]*2)+180), int(round(subset[kk,9]*2)+180)]
+                #lon = [int(round(subset[kk,10]*2)), int(round(subset[kk,11]*2))]
+                #lon[lon == 720] = 0
+                lon = int(round(subset[kk,5]*2.0))
+                lat = int(round(subset[kk,4]*2.0)) + 180
+                if lon != 720:
+                   pr = prec[jj,lat,lon] 
                 
-                # calculate the SST from the center lat/lon and time
+                # calculate the SST from the core lat/lon and time
                 sstval = sst[np.argwhere(tiempo == datetime(int(subset[kk,0]),\
                          int(subset[kk,1]),1)),int(subset[kk,4])+90,\
                          int(subset[kk,5])]
@@ -173,24 +161,20 @@ def precipCollocate4_local(ENSO):
                 # invalid sea surface temperatures are stored as -32767
                 if sstval and np.asscalar(sstval) > 200.:
                    # calculate the average and max precip from the grid, store these and MCS values
-		   ppp = np.nanmean(grid[:])
-		   qqq = np.nanmax(grid[:])
 	           sea.append(sstval)
 		   dep.append(subset[kk,6])
-                   pmean.append(ppp)
-		   pmax.append(qqq)   
+                   precip.append(pr)
 
     # store the system size [km], SST [K], mean precip rate [mm h-1], and max precip rate [mm h-1]
-    mcsprec = np.zeros((4,len(dep)))
+    mcsprec = np.zeros((3,len(dep)))
     for ii in range(len(dep)):
         mcsprec[0,ii] = dep[ii] 
         mcsprec[1,ii] = sea[ii]
-        mcsprec[2,ii] = pmean[ii]
-        mcsprec[3,ii] = pmax[ii]
+        mcsprec[2,ii] = precip[ii]
     print(mcsprec.shape)
 
     print('/rigel/home/scs2229/top-secret/MCS_clim/ausgabe/precip_clim/MSWEP/' + \
-          domain + '_SST_pavg_pmax_' + period + '_local_cores.npy')
+          domain + '_SST_pcore_' + period + '_local_cores.npy')
     np.save('/rigel/home/scs2229/top-secret/MCS_clim/ausgabe/precip_clim/MSWEP/' + \
-          domain + '_SST_pavg_pmax_' + period + '_local_cores.npy',mcsprec)
+          domain + '_SST_pcore_' + period + '_local_cores.npy',mcsprec)
 
